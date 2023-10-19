3e3f950b1a937d68be11614f323f07b8d81fdf93
hg-study-pc1 <2675263825@qq.com>
2023-10-19 23:14:08 +0800

    push-hexo-theme-matery

--- resource/shell/v3-推送-发布脚本-坚果云版本2023.10.19/xyy.sh
+++ resource/shell/v3-推送-发布脚本-坚果云版本2023.10.19/xyy.sh
@@ -2,6 +2,4 @@
 
 #注意：hexo博客文档数据有变动的话，请执行xyy命令
 #推送静态数据到ecs
-rm -rf /d/hexo/source
-cp -R /d/BaiduSyncdisk/source /d/hexo
-sh /d/hexo/xyy/xyy.sh
+sh /d/hexo/resource/shell/v3-推送-发布脚本-坚果云版本2023.10.19/xyy.sh
